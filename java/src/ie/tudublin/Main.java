402cbef352bda0c57bdc5c951ca043747b6cb5be
Grace <gracerosevalentine@gmail.com>
2023-01-30 11:36:15 +0000

    branch

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -6,7 +6,7 @@ public class Main
 	public static void helloProcessing()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
+        processing.core.PApplet.runSketch(a, new HelloProcessing());
     }
 	
 	public static void main(String[] args)

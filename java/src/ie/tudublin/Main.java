0bd847725949ea55917a8ffc0171796dddaecafc
Grace <gracerosevalentine@gmail.com>
2023-02-01 14:45:18 +0000

    lab2

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -9,6 +9,12 @@ public class Main
         processing.core.PApplet.runSketch(a, new HelloProcessing());
     }
 
+	public static void bugZap()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new BugZap());
+    }
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
@@ -17,7 +23,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		helloProcessing();
+		bugZap();
 	}
 	
 }

c89a94fabbc06faec40ca634bf158e5e011e24a4
Grace <gracerosevalentine@gmail.com>
2023-02-01 15:06:12 +0000

    empty file

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,12 +3,6 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch(a, new HelloProcessing());
-    }
-
 	public static void bugZap()
 	{
 		String[] a = {"MAIN"};

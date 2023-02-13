32fd252e12cc23db8ab7e808df4f0b698ebb5b6c
Grace <gracerosevalentine@gmail.com>
2023-02-13 12:03:50 +0000

    grid

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,24 +3,6 @@ package ie.tudublin;
 public class Main
 {
 
-	public static void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch(a, new HelloProcessing());
-    }
-
-	public static void loops()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-	public static void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-
 	public static void starMap()
 	{
 		String[] a = {"MAIN"};
--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -17,9 +17,28 @@ public class StarMap extends PApplet
 		
 	}
 
+	public void drawGrid() 
+	{
+		float border = 50.0f;
+		int count = 10;
+		float gap = (width - (border * 2.0f) )/ (float) count;
+
+		for (int i = -5; i <= 5; i++)
+		{
+			float x = border + (gap * (i + 5));
+			line(x, border, x, height - border);
+			line(border, x, width - border, x);
+
+		}
+
+
+	}
+
 		
 	public void draw()
 	{	
-		strokeWeight(2);		
+		strokeWeight(2);	
+		stroke(255);
+		drawGrid();
 	}
 }

8c894c237ac585921cc37b3a6470af2bb170feaa
Grace <gracerosevalentine@gmail.com>
2023-02-16 13:31:25 +0000

    lab4

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -1,78 +1,112 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import java.util.ArrayList;
+import processing.data.Table;
+import processing.data.TableRow;
+
 
 public class StarMap extends PApplet
 {
+
+	ArrayList<Star> star = new ArrayList<Star>();
+
 	public void settings()
 	{
 		size(500, 500);
 	}
 
+	void loadStars()
+	{
+		Table table = loadTable("\\OOP-2023\\java\\data\\HabHYG15ly.csv", "header");
+		for(TableRow r:table.rows())
+		{
+			Star s = new Star(r);
+			star.add(s);
+		}
+	}
+
 	public void setup() {
 		colorMode(HSB);
 		background(0);
-		
-		smooth();
-		
-
-
+		smooth();	
 	}
 
-
-	public void drawGrid()
-	{
-		stroke(255);
-		float border = 50.0f;
-
-		int count = 10;
-		float gap = (width - (border * 2.0f)) / (float) count;
-		for(int i = -5 ; i <= 5 ; i ++)
-		{
-			float x = border + (gap * (i + 5));
-			line(x, border, x, height - border);
-			line(border, x, width - border, x);
-		}
-		
-	}
-		
 	public void draw()
 	{	
 		strokeWeight(2);		
 
 		drawGrid();
-	}
 
-	void loadStars()
-	{
-		Table table = loadTable("HabHYG15ly.csv", "header");
-		for(TableRow r:table.rows())
-		{
-			Star s = new Star(r);
-			stars.add(s);
+		for(Star s : star) {
+			float screenX = map(s.xG, -180, -1, 0, width);
+			float screenY = map(s.yG, -100, 100, 0, height);
+			float size = map(s.absMag, -1, 15, 8, 0.5f);
+			noStroke();
+			fill(255);
+			ellipse(screenX, screenY, size, size);
+
 		}
 	}
 
-	public Star(TableRow tr)
-	{
-		this(
-			tr.getInt("Hab?") == 1, 
-			tr.getString("Display Name"), 
-			tr.getFloat("Distance"),
-			tr.getFloat("Xg"),
-			tr.getFloat("Yg"),
-			tr.getFloat("Zg"),
-			tr.getFloat("AbsMag")
-		);
+	public void drawGrid(){
+		stroke(255);
+
+		float border = 50.f;
+
+		int count = 10;
+
+		float gap = (width - (border * 2.0f)) / (float) count;
+		for (int i = -5;i <= 5; i++){
+			float x = border + (gap * (i + 5));
+			line(x, border, x, height - border);
+			line(border, x, width - border, x);
+		}
 	}
-	
-	public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag) {
-		this.hab = hab;
-		this.displayName = displayName;
-		this.distance = distance;
-		this.xG = xG;
-		this.yG = yG;
-		this.zG = zG;
-		this.absMag = absMag;
+
+
+	public void printStars(){
+		
 	}
+
+	class Star{
+		boolean hab;
+		String displayName;
+		float distance;
+		float xG;
+		float yG;
+		float zG;
+		float absMag;
+		
+
+		public Star(TableRow tr)
+		{
+			this(
+				tr.getInt("Hab?") == 1, 
+				tr.getString("Display Name"), 
+				tr.getFloat("Distance"),
+				tr.getFloat("Xg"),
+				tr.getFloat("Yg"),
+				tr.getFloat("Zg"),
+				tr.getFloat("AbsMag")
+			);
+		}
+		
+		public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag) {
+			this.hab = hab;
+			this.displayName = displayName;
+			this.distance = distance;
+			this.xG = xG;
+			this.yG = yG;
+			this.zG = zG;
+			this.absMag = absMag;
+		}
+
+		public String toString(){
+			return hab + "\t\t" + displayName; 
+		}
+	}
+
+
+
 }

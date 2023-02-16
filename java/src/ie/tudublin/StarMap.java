f103d6069bcd798e915cf6645812a29b5407adac
Grace <gracerosevalentine@gmail.com>
2023-02-16 12:31:44 +0000

    lab 4

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -42,4 +42,37 @@ public class StarMap extends PApplet
 
 		drawGrid();
 	}
+
+	void loadStars()
+	{
+		Table table = loadTable("HabHYG15ly.csv", "header");
+		for(TableRow r:table.rows())
+		{
+			Star s = new Star(r);
+			stars.add(s);
+		}
+	}
+
+	public Star(TableRow tr)
+	{
+		this(
+			tr.getInt("Hab?") == 1, 
+			tr.getString("Display Name"), 
+			tr.getFloat("Distance"),
+			tr.getFloat("Xg"),
+			tr.getFloat("Yg"),
+			tr.getFloat("Zg"),
+			tr.getFloat("AbsMag")
+		);
+	}
+	
+	public Star(boolean hab, String displayName, float distance, float xG, float yG, float zG, float absMag) {
+		this.hab = hab;
+		this.displayName = displayName;
+		this.distance = distance;
+		this.xG = xG;
+		this.yG = yG;
+		this.zG = zG;
+		this.absMag = absMag;
+	}
 }

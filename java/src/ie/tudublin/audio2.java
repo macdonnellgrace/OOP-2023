d0fe8955aba0014b07699c6346625cc23bb25026
Grace <gracerosevalentine@gmail.com>
2023-02-27 11:35:17 +0000

    audio 2

---
+++ java/src/ie/tudublin/audio2.java
@@ -0,0 +1,54 @@
+package ie.tudublin;
+
+import ddf.minim.AudioBuffer;
+import ddf.minim.AudioInput;
+import ddf.minim.AudioPlayer;
+import ddf.minim.Minim;
+import processing.core.PApplet;
+
+public class audio2 extends PApplet
+{
+    Minim minim;
+    AudioPlayer ap;
+    AudioInput ai;
+    AudioBuffer ab;
+
+    public void settings()
+    {
+        size(1024, 1000, P3D);
+        //fullScreen(P3D, SPAN);
+    }
+
+    public void setup()
+    {
+        minim = new Minim(this);
+        // Uncomment this to use the microphone
+        ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
+        ab = ai.mix; 
+
+    }
+
+
+
+    public void draw()
+    {   
+            
+    }
+
+
+        
+        // Other examples we made in the class
+        /*
+        stroke(255);
+        fill(100, 255, 255);        
+        
+        circle(width / 2, halfH, lerpedA * 100);
+
+        circle(100, y, 50);
+        y += random(-10, 10);
+        smoothedY = lerp(smoothedY, y, 0.1f);        
+        circle(200, smoothedY, 50);
+        */
+
+    }        
+}
